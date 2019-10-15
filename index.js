30a014b453edfddbb03e422abc95471bb7735430
심규빈 <simgyubin@fatoscorp.com>
2019-10-15 11:40:03 +0900

    1.3.8 release index.js Modify

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import FatosEnvMng from './src/components/FatosEnvMng';
 import FatosNativeMap from './src/components/FatosNativeMap';
 import FatosNativeNavi from './src/components/FatosNativeNavi';
+import FatosNativeMng from './src/components/FatosNativeMng';
 
-export { FatosEnvMng, FatosNativeMap, FatosNativeNavi };
+export { FatosEnvMng, FatosNativeMap, FatosNativeNavi, FatosNativeMng };
